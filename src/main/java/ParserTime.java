8b594d2dafda4c4dd179744cb3b8b33ba151f0bd
Mihail Kirsanychev <touchdreamy@gmail.com>
2021-01-03 16:20:45 +0300

    version 1.1

--- src/main/java/ParserTime.java
+++ src/main/java/ParserTime.java
@@ -10,56 +10,24 @@ public class ParserTime
 {
     public static Document getPage(String city) throws IOException
     {
-        String url = "https://time100.ru/" + city;
-        Document page = Jsoup.parse(new URL(url), 2000);
+        Document page = null;
+        try {
+            String url = "https://time100.ru/" + city;
+            page = Jsoup.parse(new URL(url), 2000);
+        } catch (Exception e)
+        {
+            System.out.println("Такой город не найден");
+            System.exit(1);
+        }
         return page;
     }
 
     public static String getCity()
     {
-        String[] citys = new String[] {"London", "Tokyo", "New-York", "Hong-Kong", "Sydney", "Tel-Aviv", "Kyiv",
-                                        "Moscow", "Saint-Petersburg", "Nizhny-Novgorod", "Yekaterinburg", "Saratov",
-                                        "Novosibirsk", "Vladivostok", "Omsk", "Volgograd", "Kaliningrad", "Krasnoyarsk",
-                                        "Magadan", "Samara", "Irkutsk", "Kamchatka", "Stavropol"};
-
-        System.out.println("Select number of city where you want to know the time:");
-        for (int i=0; i < citys.length; i++)
-        {
-            System.out.println(i+1 + " - " + citys[i]);
-        }
-
-        System.out.println("");
-        int num = inputNumber(1, citys.length);
-
-        System.out.println("\n" + citys[num - 1]);
-        return (citys[num - 1]);
-    }
-
-    public static int inputNumber(int startIndex, int endIndex)
-    {
         Scanner sc = new Scanner(System.in);
-        int num;
-
-        do {
-            System.out.print("Enter the number of the city: ");
-
-            while (!sc.hasNextInt())
-            {
-                System.out.println("Error! That not a number! Try again.");
-                System.out.print("Enter the number of the city: ");
-                sc.next(); // this is important!
-            }
-
-            num = sc.nextInt();
-
-            if (num < startIndex || num > endIndex)
-            {
-                System.out.println("Error! The wrong number was entered. Try again.");
-            }
-
-        } while (num < startIndex || num > endIndex);
-
-        return num;
+        System.out.print("Введите название города: ");
+        String city = sc.nextLine();
+        return city;
     }
 
 
